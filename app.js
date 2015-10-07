d7a10fc49cec84f02001e10df147ed77086453f3
Andrew <yu.drew@gmail.com>
2015-10-07 16:06:22 -0700

    Create angular app

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ var app = angular.module('anticipate', []);
 
 app.controller('MainCtrl', [
 	'$scope', function($scope){
+
 		$scope.test = 'Hello World!';
+
+		$scope.posts = [
+		  {title: 'post 1', upvotes: 5},
+		  {title: 'post 2', upvotes: 2},
+		  {title: 'post 3', upvotes: 20},
+		];
+
 	}
 ]);
